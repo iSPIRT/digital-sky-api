46879840ef38d0e257767eef31e97c58d4a7b397
archanaa2 <archana@sahajsoft.com>
2018-07-17 22:39:10 +0530

    [DS-0010]

--- src/main/java/com/ispirit/digitalsky/controller/LocalDroneAcquisitionFormRestController.java
+++ src/main/java/com/ispirit/digitalsky/controller/LocalDroneAcquisitionFormRestController.java
@@ -2,7 +2,6 @@ package com.ispirit.digitalsky.controller;
 
 import com.ispirit.digitalsky.domain.ApproveRequestBody;
 import com.ispirit.digitalsky.domain.LocalDroneAcquisitionApplicationForm;
-import com.ispirit.digitalsky.domain.LocalDroneAcquisitionApplicationFormWithUploadFiles;
 import com.ispirit.digitalsky.repository.EntityRepository;
 import com.ispirit.digitalsky.repository.LocalDroneAcquisitionFormRepository;
 import com.ispirit.digitalsky.repository.storage.StorageFileNotFoundException;
