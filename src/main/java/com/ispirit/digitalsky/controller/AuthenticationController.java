2ab3905e32eddb618ac32e060308943d2d3d6c97
CharanSahaj <charana@sahajsoft.com>
2018-12-11 16:53:39 +0530

    adding api to test if admin

--- src/main/java/com/ispirit/digitalsky/controller/AuthenticationController.java
+++ src/main/java/com/ispirit/digitalsky/controller/AuthenticationController.java
@@ -15,16 +15,15 @@ import com.ispirit.digitalsky.service.api.UserProfileService;
 import com.ispirit.digitalsky.util.AuthenticationUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -72,5 +71,11 @@ public class AuthenticationController {
         String accessToken = setSecurityUserSecurityContext(securityTokenService, authentication);
 
         return generateTokenResponse(userPrincipal, userProfileService.profile(userPrincipal.getId()), accessToken);
+    }
+
+    @RequestMapping(value = "/verifyAdmin",method = RequestMethod.GET)
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> verifyIfadmin(@Valid @RequestHeader String Authorization){
+        return new ResponseEntity<>(null, HttpStatus.OK);
     }
 }
